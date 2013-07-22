a27bc6acc51d3898af8dd57a047083b00deaf6a4
andylindemann <pz81ndl>
2013-07-22 23:34:57 +0200

    more

--- meBookmarklet.js
+++ meBookmarklet.js
@@ -1,32 +1,34 @@
 //javascript:
-    //(
-document.getElementById('bm').addEventListener('click',booky);
-      var booky = function () {
-        window.__MEFrame = document.createElement('iframe');
-        __MEFrame.src = "http://10.0.0.2:3000/"+encodeURIComponent(window.location.href);
+//(
+document.getElementById('bm').addEventListener('click', function(){
+    console.log('click');
+    booky();
 
-        __MEFrame.basicCSS = "position:fixed;top:0;right:0;border:none;z-index:2147483646;border:none;"+['-webkit-','-moz-','-ms-','-o-',''].join('transition:all .1s;');
-        __MEFrame.style.cssText = __MEFrame.basicCSS+"width:60px;height:30px";
-        var d = document.createElement('div');
-        d.innerHTML='open';
-        d.style.cssText='width:50px;height:20px;position:fixed;top:5px;right:5px;background-color:#fb8;z-index:2147483647';
-        d.onclick=function(){
-            this.innerHTML = this.innerHTML==='open'?'close':'open';
-            var s = __MEFrame.style;
-            console.log('click');
-            console.log(s.width);
-            if(s.width === '500px'){
-                s.cssText=__MEFrame.basicCSS+'width:60px;height:30px;'
 
-            }else{
-                s.cssText=__MEFrame.basicCSS+'width:500px;height:200px;'
+});
 
-            }
-
-        };
-        document.body.appendChild(__MEFrame);
-        document.body.appendChild(d);
-
-    }
+var booky = function () {
+    if (typeof window.__MEFrame ==='undefined'){
+    window.__MEFrame = document.createElement('iframe');
+    __MEFrame.src = "http://message-everywhere.meteor.com/" + encodeURIComponent(window.location.href);
+    __MEFrame.basicCSS = "border-bottom-left-radius:10px;position:fixed;top:0;right:0;border:none;z-index:2147483646;border:none;" + ['-webkit-', '-moz-', '-ms-', '-o-', ''].join('transition:all .1s;');
+    __MEFrame.style.cssText = __MEFrame.basicCSS + "width:50px;height:50px";
+    __MEFrame.open = false;
+    var d = document.createElement('div');
+    d.innerHTML = '&nabla;';
+    d.style.cssText = 'width:30px;height:30px;position:fixed;top:10px;right:10px;background-color:#fb8;z-index:2147483647';
+    d.onclick = function () {
+        this.innerHTML = __MEFrame.open ? '&nabla;':'&Delta;';
+        var s = __MEFrame.style;
+        if (__MEFrame.open) {
+            s.cssText = __MEFrame.basicCSS + 'width:50px;height:50px;'
+        } else {
+            s.cssText = __MEFrame.basicCSS + 'width:500px;height:500px;'
+        }
+        __MEFrame.open = !__MEFrame.open;
+    };
+    document.body.appendChild(__MEFrame);
+    document.body.appendChild(d);
+}};
 
 //)();
