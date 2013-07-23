3efa58c2f1233df02e44d6ae2c2794817f0f56f2
AndyTheGiant <andy.bialystock@gmail.com>
2013-07-23 15:17:40 +0200

    Update meBookmarklet.js

--- meBookmarklet.js
+++ meBookmarklet.js
@@ -12,14 +12,13 @@ var booky = function () {
     if (typeof window.__MEFrame ==='undefined'){
         window.__MEFrame = document.createElement('iframe');
         __MEFrame.src = "http://message-everywhere.meteor.com/" + encodeURIComponent(window.location.href);
+        __MEFrame.scrolling = 'no';
         __MEFrame.basicCSS = "border-bottom-left-radius:10px;position:fixed;top:0;right:0;border:none;z-index:2147483646;border:none;" + ['-webkit-', '-moz-', '-ms-', '-o-', '',''].join('transition:all .1s;');
         __MEFrame.style.cssText = __MEFrame.basicCSS + "width:50px;height:50px";
         __MEFrame.open = false;
         var d = document.createElement('div');
-        d.innerHTML = '&nabla;';
-        d.style.cssText = 'width:30px;height:30px;position:fixed;top:10px;right:10px;background-color:#fb8;z-index:2147483647';
+        d.style.cssText = 'width:50px;height:50px;position:fixed;top:0;0;z-index:2147483647';
         d.addEventListener('click',function () {
-            this.innerHTML = __MEFrame.open ? '&nabla;':'&Delta;';
             var s = __MEFrame.style;
             if (__MEFrame.open) {
                 s.cssText = __MEFrame.basicCSS + 'width:50px;height:50px;';
